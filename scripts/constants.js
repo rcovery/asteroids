a78292aa5fee5772207c2e586f8ae66bf47d567d
Ryan <ryancpereira@protonmail.com>
2023-09-01 20:08:13 -0300

    Constants added

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,9 @@
 export const SCREEN_WIDTH = 800;
 export const SCREEN_HEIGHT = 600;
+
+export const MOVE_UP = 'w';
+export const MOVE_DOWN = 's';
+export const MOVE_LEFT = 'a';
+export const MOVE_RIGHT = 'd';
+
+export const PLAYER_MAX_SPEED = 10;
